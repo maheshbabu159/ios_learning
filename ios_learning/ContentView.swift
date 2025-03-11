651541592d2ba895d6b173afc8fa3aa844853bd5
MAHESHBABU SOMINENI <maheshbabu.somineni@gmail.com>
2025-03-11 14:10:43 -0500

    Initial Commit

---
+++ ios_learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_learning
+//
+//  Created by Maheshbabu Somineni on 3/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_learning/ios_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_learningApp.swift
+//  ios_learning
+//
+//  Created by Maheshbabu Somineni on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
